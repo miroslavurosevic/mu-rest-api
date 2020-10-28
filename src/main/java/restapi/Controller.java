cabab3f9be95dcc3a19fe375827778b688ab5bad
Miroslav Urosevic <miroslav.urosevic.gh@gmail.com>
2020-10-28 19:56:21 +0200

    Initial commit

---
+++ src/main/java/restapi/Controller.java
@@ -0,0 +1,25 @@
+package restapi;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Controller {
+		
+	@GetMapping("/getMovie")
+	public String getMovie(@RequestParam String title,@RequestParam(required=false) String year,@RequestParam String plotType ){
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity("http://www.omdbapi.com/?t="+title+"&y="+year+"&plot="+plotType+"&apikey=fcd9ca9d", String.class);
+		return response.getBody();
+	}
+	
+	@GetMapping("/getBook")
+	public String getMovie(@RequestParam String isbn) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity("http://openlibrary.org/search.json?isbn="+isbn, String.class);
+		return response.getBody();
+	}
+}
---
+++ src/main/java/restapi/user/ApplicationUserRepository.java
@@ -0,0 +1,13 @@
+package restapi.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+	
+	
+	ApplicationUser findByUsername(String username);
+	
+}
+
